2af44e278d8b4406261232112a15dfa060d95709
AurelienMarchal <marchal.aure@gmail.com>
2026-08-23 13:11:40 +0200

    On peut faire des passes omg

--- Scripts/Game/Ball.cs
+++ Scripts/Game/Ball.cs
@@ -8,11 +8,18 @@ public partial class Ball : RigidBody3D
 		get;
 		set;
 	}
+
+	public int controlledByPlayer{
+		get;
+		set;
+	}
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		free = true;
+		controlledByPlayer = -1;
+		FreezeMode = FreezeModeEnum.Kinematic;
 		
 	}
 
@@ -22,5 +29,28 @@ public partial class Ball : RigidBody3D
 		
 
 		
+	}
+
+	public bool TryToBecomeControlledByPlayer(int playerNum)
+	{	
+		if(controlledByPlayer >= 0)
+		{
+			return false;
+		}
+		free = false;
+		controlledByPlayer = playerNum;
+		Freeze = true;
+
+		GD.Print($"Ball Controlled by player {playerNum}");
+
+		return true;
+	}
+
+	public void BecomeFree()
+	{
+		free = true;
+		Freeze = false;
+		controlledByPlayer = -1;
+		GD.Print("Ball Free");
 	}
 }
--- Scripts/Game/Player.cs
+++ Scripts/Game/Player.cs
@@ -3,30 +3,180 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
-    
-    private float direction;
+
+    [Export]
+    public bool controlledByInput;
 
     [Export]
     private float walkingSpeed;
 
+    [Export]
+    private float passShootPower;
+
+    [Export]
+    private float controlledBallDistanceToCenter;
+
+    [Export]
+    private float controlledBallY;
+
+    [Export]
+    private int playerNum;
+
+    Timer timerBeforeAbleToControlBall;
+
+    Area3D ballDetectionArea;
+
+    public Ball controlledBall{
+		get;
+		private set;
+	}
+
     public override void _Ready()
 	{
-		
+		controlledBall = null;
+        timerBeforeAbleToControlBall = GetNode<Timer>("./TimerBeforeAbleToControlBall");
+        ballDetectionArea = GetNode<Area3D>("./BallDetectionArea");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 inputVector = Input.GetVector("MovePlayerDown", "MovePlayerUp", "MovePlayerLeft", "MovePlayerRight");
+        if(controlledBall == null)
+        {
+            var overlappingBodies = ballDetectionArea.GetOverlappingBodies();
+            foreach (var body in overlappingBodies)
+            {
+                if(body is Ball ball) 
+                {
+                    TryToGainControlOfBall(ball);
+                }
+            }
+        }
 
-        Velocity = new Vector3(walkingSpeed * inputVector.X * (float)delta, 0f, walkingSpeed * inputVector.Y * (float)delta);
+        if (controlledByInput)
+        {
+            Vector2 inputVector = Input.GetVector(
+                "MovePlayerDown", 
+                "MovePlayerUp", 
+                "MovePlayerLeft", 
+                "MovePlayerRight"
+            );
+            if (inputVector != Vector2.Zero)
+            {
+                var inputVectorNormalized = inputVector.Normalized();
+
+                GlobalRotation = new Vector3(
+                    0f,
+                    Mathf.Atan2(inputVector.Y, inputVector.X),
+                    0f
+                );
+
+                Velocity = new Vector3(
+                    walkingSpeed * inputVectorNormalized.X * (float)delta,
+                    0f,
+                    walkingSpeed * inputVectorNormalized.Y* (float)delta
+                );
+            }
+            else
+            {
+                Velocity = Vector3.Zero;
+            }
+        }
+        else
+        {
+            
+        }
+
         MoveAndSlide();
-        
+        MoveControlledBall();
+
 	}
 
-
-    public void OnBallDetectionAreaBodyEntered(Node3D body)
+    public override void _Input(InputEvent @event)
     {
-        GD.Print(body);
+       
+        if (@event.IsActionPressed("MakePass"))
+        {
+            GD.Print("MakePass");
+            ShootControlledBall(passShootPower);
+        }
     }
+
+    public void TryToGainControlOfBall(Ball ball)
+    {
+        var didSucceed = ball.TryToBecomeControlledByPlayer(playerNum);
+        if (didSucceed)
+        {
+             controlledBall = ball;
+            SetCollisionMaskValue(3/*Ball*/, false);
+            ballDetectionArea.SetCollisionMaskValue(3/*Ball*/, false);
+        }
+       
+    }
+
+    public void LooseControlOfBall()
+    {
+        controlledBall.BecomeFree();
+        controlledBall = null;
+        timerBeforeAbleToControlBall.Start();
+    }
+
+    public void OnTimerBeforeAbleToControlBallTimeout()
+    {
+        GD.Print($"Setting back collision with ball");
+        SetCollisionMaskValue(3/*Ball*/, true);
+        ballDetectionArea.SetCollisionMaskValue(3/*Ball*/, true);
+    }
+
+    public void MoveControlledBall()
+    {
+        if(controlledBall == null)
+        {
+            return;
+        }
+
+        controlledBall.GlobalPosition = 
+            new Vector3(
+                GlobalPosition.X, 
+                controlledBallY, 
+                GlobalPosition.Z
+            ) + 
+            new Vector3(
+                controlledBallDistanceToCenter * Mathf.Cos(GlobalRotation.Y), 
+                0f, 
+                controlledBallDistanceToCenter * Mathf.Sin(GlobalRotation.Y)
+            );
+    }
+
+    public void ShootControlledBall(float power)
+    {
+        if(controlledBall == null)
+        {
+            return;
+        }
+
+        if(power <= 0f)
+        {
+            return;
+        }
+
+        var impulse = new Vector3(
+            power * Mathf.Cos(GlobalRotation.Y), 
+            0f, 
+            power * Mathf.Sin(GlobalRotation.Y)
+        );
+
+        GD.Print($"Shooting with impulse {impulse}");
+
+        controlledBall.BecomeFree();
+        
+        timerBeforeAbleToControlBall.Start();
+
+        controlledBall.ApplyCentralImpulse(impulse);
+        
+        controlledBall = null;
+        
+    }
+
+    
 }
